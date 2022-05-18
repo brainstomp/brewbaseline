90cf6c0514d8a9007c98c6b921e925422f113f2e
brainstomp <42446297+brainstomp@users.noreply.github.com>
2022-05-18 10:34:30 -0400

    Create installmytools.sh

---
+++ installmytools.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+brew install --cask slack
+brew install --cask sublime-text
+brew install --cask lastpass
+brew install --cask audacity
+brew install --cask balenaetcher
+brew install --cask brave-browser
+brew install --cask discord
+brew install --cask expressvpn
+brew install --cask firefox
+brew install --cask iterm2
+brew install --cask lastpass
+brew install --cask vlc
+brew install --cask vnc-viewer
