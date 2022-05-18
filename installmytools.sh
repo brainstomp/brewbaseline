ad4494e72541f4cec1884a9f07a69c23d74025a2
brainstomp <42446297+brainstomp@users.noreply.github.com>
2022-05-18 10:54:12 -0400

    Update installmytools.sh
    Cleaned up the formatting and added stuff that was not part of my prior baseline.

--- installmytools.sh
+++ installmytools.sh
@@ -1,15 +1,47 @@
 #!/bin/sh
 
+# Chat/messaging
 brew install --cask slack
-brew install --cask sublime-text
-brew install --cask lastpass
-brew install --cask audacity
-brew install --cask balenaetcher
-brew install --cask brave-browser
 brew install --cask discord
-brew install --cask expressvpn
-brew install --cask firefox
-brew install --cask iterm2
+
+# Text editing
+brew install --cask sublime-text
+
+# Security
 brew install --cask lastpass
+brew install --cask expressvpn
+brew install tailscale
+brew install --cask watchguard-mobile-vpn-with-ssl
+
+# Media editing
+brew install --cask audacity
 brew install --cask vlc
+brew install handbrake
+brew install --cask obs
+brew install --cask openshot-video-editor
+
+# Browsers
+brew install --cask brave-browser
+brew install --cask firefox
+brew install --cask vivaldi
+brew install --cask tor-browser
+brew install --cask hush
+
+# Command line tools
+brew install --cask iterm2
+brew install glances
+brew install nmap
+brew install --cask sequel-ace
+
+# Remote Control tools
 brew install --cask vnc-viewer
+brew install --cask microsoft-remote-desktop
+
+# Drive/Disk imaging 
+brew install --cask raspberry-pi-imager
+brew install --cask balenaetcher
+
+# Utilities
+brew install --cask bartender
+brew install --cask itsycal
+brew install --cask rectangle
